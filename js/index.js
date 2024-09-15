7192b998f8eb8e4dfbf8f4654a80958e0554e4fc
lekcik <cultcrib@gmail.com>
2024-09-15 11:20:47 +0200

    weather preview layout completed

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
 async function getData(url) {
-    const response = await fetch(url)
-    const data = await response.json()
-    return data
-}
-
-async function getIpAddress() {
     try {
-      const response = await fetch('https://api.ipify.org?format=json')
-      const data = await response.json()
-      return data.ip
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');  
+        }
+        const data = await response.json();
+        return data;
     } catch (error) {
-      console.error('Error fetching IP address:', error)
+        alert('No internet connection or failed to fetch data!');
+        console.error('Error:', error); 
     }
 }
 
 async function runApp() {
-    const APIKey = `4026144c88e744a8b18160722241309`
-    let desiredLocation = await getIpAddress()
-    let url = `http://api.weatherapi.com/v1/current.json?key=${APIKey}&q=${desiredLocation}`
+    const APIKey = `4026144c88e744a8b18160722241309`;
+    let desiredLocation = `London`;
+    let url = `https://api.weatherapi.com/v1/current.json?key=${APIKey}&q=${desiredLocation}`;
 
-    let data = await getData(url)
+    let data = await getData(url);
 
-    console.log(data.current.condition.icon)
+    if (data) {
+    
+    }
 }
